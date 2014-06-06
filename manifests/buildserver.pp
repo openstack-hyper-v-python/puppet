8ae8965feead518b023a654f48c812bd910a8345
paranoidgabe <gloewen@crimson.ua.edu>
2014-06-06 10:15:31 -0700

    changed file name

---
+++ manifests/buildserver.pp
@@ -0,0 +1,20 @@
+node /buildserver.*/ {
+
+    include jenkins
+    jenkins::plugin {
+        'git':  ;
+        'git-client':   ;
+        'github-api':   ;
+        'github':   ;
+    }
+    class {'jenkins_job_builder:'}
+    class {'mingw:'}
+    class {'windows_git':}
+    class {'visualcplusplus2008:'}
+    class {'swig:'}
+    class {'nasm':}
+    class {'windows_openssl': 
+        $openssl_path => 'C:\\pkg',
+    }
+    
+}
